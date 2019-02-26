c386629a83c4b5fe9e168a17705c740581c4633b
Pascal Boeschoten <passiepascal@gmail.com>
2019-02-26 20:48:17 +0100

    Add example for adding a run

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/go-openapi/strfmt"
 	//"github.com/go-openapi/spec"
 
 	"github.com/SoftwareForScience/jiskefet-api-go/client/runs"
 	runsclient "github.com/SoftwareForScience/jiskefet-api-go/client/runs"
+	"github.com/SoftwareForScience/jiskefet-api-go/models"
 
 	//apiclient "github.com/SoftwareForScience/jiskefet-api-go/client"
 	httptransport "github.com/go-openapi/runtime/client"
@@ -22,7 +24,7 @@ func main() {
 	apiPath := os.Getenv("JISKEFET_PATH")
 	apiToken := os.Getenv("JISKEFET_API_TOKEN")
 	fmt.Printf("JISKEFET_HOST:      %s\n", hostUrl)
-	fmt.Printf("JISKEFET_API_PATH:  %s\n", apiPath)
+	fmt.Printf("JISKEFET_PATH:  %s\n", apiPath)
 	fmt.Printf("JISKEFET_API_TOKEN: %s\n", apiToken)
 
 	// create the API client
@@ -35,13 +37,59 @@ func main() {
 	// apiKeyQueryAuth := httptransport.APIKeyAuth("apiKey", "query", os.Getenv("API_KEY"))
 	// apiKeyHeaderAuth := httptransport.APIKeyAuth("X-API-TOKEN", "header", os.Getenv("API_KEY"))
 
-	params := runs.NewGetRunsParams()
-	resp, err := client.GetRuns(params, bearerTokenAuth)
-	// resp, err := client.Operations.All(operations.AllParams{}, basicAuth)
-	// resp, err := client.Operations.All(operations.AllParams{}, apiKeyQueryAuth)
-	// resp, err := client.Operations.All(operations.AllParams{}, apiKeyHeaderAuth)
-	if err != nil {
-		log.Fatal(err)
+	{
+		startt, err := time.Parse(time.RFC3339, "2007-03-01T13:40:45Z")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		endt, err := time.Parse(time.RFC3339, "2016-04-01T10:22:02Z")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		start := strfmt.DateTime(startt)
+		end := strfmt.DateTime(endt)
+		runType := "my-run-type"
+		runQuality := "my-run-quality"
+		activityId := "go-api"
+		nDetectors := int64(123)
+		nFlps := int64(200)
+		nEpns := int64(1000)
+		nTimeframes := int64(1231)
+		nSubtimeframes := int64(12312)
+		bytesReadOut := int64(1024 * 1024)
+		bytesTimeframeBuilder := int64(512 * 1024)
+
+		params := runs.NewPostRunsParams()
+		params.CreateRunDto = new(models.CreateRunDto)
+		params.CreateRunDto.TimeO2Start = &start
+		params.CreateRunDto.TimeTrgStart = &start
+		params.CreateRunDto.TimeO2End = &end
+		params.CreateRunDto.TimeTrgEnd = &end
+		params.CreateRunDto.RunType = &runType
+		params.CreateRunDto.RunQuality = &runQuality
+		params.CreateRunDto.ActivityID = &activityId
+		params.CreateRunDto.NDetectors = &nDetectors
+		params.CreateRunDto.NFlps = &nFlps
+		params.CreateRunDto.NEpns = &nEpns
+		params.CreateRunDto.NTimeframes = &nTimeframes
+		params.CreateRunDto.NSubtimeframes = &nSubtimeframes
+		params.CreateRunDto.BytesReadOut = &bytesReadOut
+		params.CreateRunDto.BytesTimeframeBuilder = &bytesTimeframeBuilder
+
+		_, err = client.PostRuns(params, bearerTokenAuth)
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
-	fmt.Printf("%#v\n", resp.Payload)
+
+	{
+		params := runs.NewGetRunsParams()
+		resp, err := client.GetRuns(params, bearerTokenAuth)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%#v\n", resp.Payload)
+	}
 }
